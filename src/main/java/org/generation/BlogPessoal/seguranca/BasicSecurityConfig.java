b8a00731df44f793968fecfe0dabcf74b7b7336a
vnalmenara <veronica_n_almenara@hotmail.com>
2021-03-01 18:06:05 -0300

    Alteracao-basic-security-config

--- src/main/java/org/generation/BlogPessoal/seguranca/BasicSecurityConfig.java
+++ src/main/java/org/generation/BlogPessoal/seguranca/BasicSecurityConfig.java
@@ -31,10 +31,10 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers("/usuarios/logar").permitAll()
+		.antMatchers("/**").permitAll()
 		.antMatchers("/usuarios/cadastrar").permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
-		.and().sessionManagement()
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and().cors()
 		.and().csrf().disable();
